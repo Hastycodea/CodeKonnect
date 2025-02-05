e482b62b5127d84d2d2d87ea04af9d98424721c6
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-02-05 10:22:41 +0300

    somUpdates

--- SomaBro/script.js
+++ SomaBro/script.js
@@ -2,3 +2,12 @@ let content = document.querySelectorAll('.sub-content')
 console.log(content)
 
 content[0].classList.add('large')
+
+let doneButton = document.querySelectorAll('.sub-content')
+console.log(doneButton)
+
+content.forEach((cont, i) => {
+    doneButton[i].addEventListener("click", function() {
+        content[i].add('large')
+    })
+})
