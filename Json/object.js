9100d621df8ea8147d4c99fb506e69bbc3fb40e7
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-02-07 10:50:13 +0300

    object+json

---
+++ Json/object.js
@@ -0,0 +1,19 @@
+const person = {
+  firstName: "Asabeneh",
+  lastName: "Yetayeh",
+  age: 250,
+  country: "Finland",
+  city: "Helsinki",
+  skills: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node",
+    "MongoDB",
+    "Python",
+    "D3.js",
+  ],
+};
+
+console.log(person.skills)
--- Json/src.js
+++ Json/src.js
@@ -21,10 +21,47 @@ let people = {
   ],
 };
 
+//structure of Json
+//JSON.stringify(value, replacer, space)
+console.log(JSON.stringify(people, undefined, 4))
+// console.log(JSON.stringify(people, ['users']))
 // console.log(people)
-console.log(people.users[0].firstName)
-console.log(people.users.length)
+console.log(people.users[0].firstName);
+console.log(people.users.length);
 
-for(let i=0; i<people.users.length; i++) {
-    console.log(people.users[i].firstName)
+for (let i = 0; i < people.users.length; i++) {
+  console.log(people.users[i].firstName);
 }
+
+const usersText = `{
+"users":[
+  {
+    "firstName":"Asabeneh",
+    "lastName":"Yetayeh",
+    "age":250,
+    "email":"asab@asb.com"
+  },
+  {
+    "firstName":"Alex",
+    "lastName":"James",
+    "age":25,
+    "email":"alex@alex.com"
+  },
+  {
+  "firstName":"Lidiya",
+  "lastName":"Tekle",
+  "age":28,
+  "email":"lidiya@lidiya.com"
+  }
+]
+}`;
+
+console.log(typeof(usersText))
+console.log(JSON.parse(usersText).users[0].firstName)
+
+let usersObject = JSON.parse(usersText, (key, value) => {
+    let newValue = typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+    return newValue
+})
+
+console.log(usersObject)
